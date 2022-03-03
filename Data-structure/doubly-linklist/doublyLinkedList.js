0fed9dbb4f5530c9959f0b8792e38f2c6799c127
aadil42 <adilkelawala@gmail.com>
2022-03-03 18:28:34 +0530

    added insert at position method in the structure.

--- Data-structure/doubly-linklist/doublyLinkedList.js
+++ Data-structure/doubly-linklist/doublyLinkedList.js
@@ -1,11 +1,13 @@
 // insert at start
 // insert at end
+// print list
+// print reverse list
+
 // insert at position
 // delete at start
 // delete at end
 // delete at index
-// display list
-// make list
+
 
 class node{
 
@@ -19,10 +21,10 @@ class node{
 class doublLinkList {
     constructor() {
         this.head = null;
-        // this.next = null;
         this.tail = null;
         this.size = 0;
     }
+    // inserts node at the first position
     insertFirst(data) {
         this.head = new node(data, this.head, null);
         let currunt = this.head;
@@ -54,7 +56,26 @@ class doublLinkList {
         
         this.size++;
     }
-    // print list from the end 
+    // insert at one based position;
+    insertAt1Based(data, pos) {
+        console.log(pos, this.size);
+
+        if(pos > this.size || pos <= 0) return 'please provide valid position';
+
+        if(pos == 1) return this.insertFirst(data);
+
+        // insert somewhere between
+        let currunt = this.head;
+        let count = 1;
+        while(count < (pos - 1)) {
+            currunt = currunt.next;
+            count++;
+        } 
+        const temp = currunt.next;
+        currunt.next = new node(data, temp, currunt);
+        temp.pre = currunt.next;
+    }
+    // print the list
     printList() {
         let list = '';
         let currunt = this.head;
@@ -64,7 +85,7 @@ class doublLinkList {
         }
         return list;
     }
-    // print reverse 
+    // print list in reverse 
     printReverrse() {
        let list = '';
        let currunt = this.tail;
@@ -81,7 +102,8 @@ const myll = new doublLinkList();
 myll.insertLast(100);
 myll.insertLast(200);
 myll.insertLast(300);
-myll.insertFirst(400);
-myll.insertFirst(500);
+myll.insertLast(400);
+myll.insertLast(500);
+myll.insertAt1Based(555, 1);
 console.log(myll.printList());
 console.log(myll.printReverrse());
