b6652fa543b4dabf45ab4231378d02dbef787005
aadil42 <adilkelawala@gmail.com>
2022-03-11 17:16:08 +0530

    implemented reverseList method

--- Data-structure/circular-doubly-linklist/circular-doubly-linklist.js
+++ Data-structure/circular-doubly-linklist/circular-doubly-linklist.js
@@ -1,8 +1,7 @@
 // insert at first 
 // print list
-
-
 // insert at last
+
 // reverse list
 // find size
 
@@ -30,6 +29,7 @@ class circularDoublyLinkList {
         this.head = new Node(data, null, null);
         this.head.next = this.head;
         this.head.pre = this.head;
+        this.tail = this.head;
         this.incrementSize();
     }
     
@@ -37,24 +37,41 @@ class circularDoublyLinkList {
        if(this.size == 0) return this.insertInitialNode(data);
     
        let currunt = this.head;
-        // get the tail node;
+        // get the tail node; 1->
        while(currunt && currunt.next !== this.head) {
            currunt = currunt.next;
        }
-       this.head = new Node(data, this.head, currunt);
+       this.tail = currunt;
+       this.head = new Node(data, this.head, this.tail);
        currunt.next = this.head;
-       console.log(this.head);
        this.incrementSize();    
     }
 
     insertAtLast(data) {
-        // get the last node 1->2->3->4
+        // get the last node 
+        this.tail.next = new Node(data, this.head, this.tail);
+        this.tail =  this.tail.next;
+        this.incrementSize();    
+    }
+    // 1->2->3->4
+    reverseList() {
+        let pre =  this.tail;
         let currunt = this.head;
-        while(currunt && currunt.next !== this.head) {
-            currunt = currunt.next;
+        while(currunt.next !== this.head) {
+            const temp = currunt.next;
+            currunt.next = pre;
+            pre.pre = currunt;
+            pre = currunt;
+            currunt = temp;
+        } 
+        // taking care of the last node;
+        if(currunt.next == this.head) {
+            currunt.next = pre;
+            pre.pre = currunt;
         }
-
-        currunt.next = new Node(data, this.head, currunt);
+        const temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
     }
 
     printList() {
@@ -69,18 +86,33 @@ class circularDoublyLinkList {
         }
         return output;
     }
+    printFormEnd() {
+        let currunt = this.tail;
+        let output = '';
+        while(currunt.pre !== this.tail) {
+            output += currunt.val + '->';
+            currunt = currunt.pre;
+        }
+        if(currunt.pre == this.tail) {
+            output += currunt.val + '->';
+        }
+        return output;
+    }
 }
 
 
 const dcll = new circularDoublyLinkList();
 
+dcll.isnertAtFirst(1);
 dcll.isnertAtFirst(2);
 dcll.isnertAtFirst(3);
 dcll.isnertAtFirst(3);
 dcll.insertAtLast(4);
+dcll.insertAtLast(5);
 console.log(dcll.printList());
+dcll.reverseList();
+console.log(dcll.printList());
+console.log(dcll.printFormEnd());
 
-// dcll.isnertAtFirst(1);
-// dcll.isnertAtFirst(3);
-// dcll.isnertAtFirst(4);
+
 
