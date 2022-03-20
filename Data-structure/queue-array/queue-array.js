f42a03d9a1e6e7dadd2a7cbbc84fb00017d4665e
aadil42 <adilkelawala@gmail.com>
2022-03-20 21:59:02 +0530

    implemented queue-array data structure

---
+++ Data-structure/queue-array/queue-array.js
@@ -0,0 +1,63 @@
+// - Enqueue() – Add item to the queue from the REAR.
+// - Dequeue() – Remove item from the queue from the FRONT.
+// - isFull() – Check if queue is full or not.
+// - isEmpty() – Check if queue empty or not.
+// - count() – Get number of items in the queue.
+// - peek() – return front elemenet in the queue(line).
+// 1 2 3 4 5 6 7 8 9 10 
+
+
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.size = 0;
+    }
+
+    enqueue(data) {
+        this.queue.push(data);
+        this.size++;
+        return this.queue;
+    }
+
+    dequeue() {
+       this.front++;
+       this.size--;
+    }
+
+    isEmpty() {
+        if(this.front > this.queue.length - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    count() {
+        return this.size;
+    }
+
+    peek()  {
+        return this.queue[this.front] ? this.queue[this.front] : 'nothing is in the queue';
+    }
+
+    isFull() {
+        if(this.front < this.queue.length - 1) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
+
+
+const myQueue = new Queue();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+myQueue.enqueue(4);
+myQueue.enqueue(5);
+myQueue.dequeue();
+myQueue.dequeue();
+
+console.log(myQueue.count());
