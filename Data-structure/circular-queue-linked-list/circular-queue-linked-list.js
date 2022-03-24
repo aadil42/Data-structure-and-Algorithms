303910734ed087e998493c4fdaa267e080af414c
aadil42 <adilkelawala@gmail.com>
2022-03-24 15:32:48 +0530

    got rid of unnecessary code

--- Data-structure/circular-queue-linked-list/circular-queue-linked-list.js
+++ Data-structure/circular-queue-linked-list/circular-queue-linked-list.js
@@ -13,64 +13,6 @@ class Node {
     }
 }
 
-// class Circular_queue_linked_list {
-//     constructor(size) {
-//         this.size = size;
-//         this.front = null;
-//         this.rear = null;
-//         this.counter = 0;
-//     }
-//     // 1->2->3->4  5
-//     enqueue(data) {
-//         if(this.isFull()) {
-//             return;
-//         } else if(this.empty()) {
-//             this.front = new Node(data);
-//             this.rear = this.front;
-//             this.rear.next = this.front;
-//             this.counter++;
-//         } else {
-//             const dataNode = new Node(data);
-//             const temp = this.rear.next;
-//             this.rear.next = dataNode;
-//             this.rear = dataNode;
-//             dataNode.next = temp; 
-//             this.counter++;
-//         }
-//     }
-//     dequeu() {
-//         this.front = this.front.next;
-//         this.counter--;
-//     }
-//     empty() {
-//         if(this.counter === 0) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
-
-//     isFull() {
-//         if(this.counter === this.size) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
-
-//     print() {
-//         let output = '';
-//         let currunt = this.front;
-//         while(currunt.next !== this.front) {
-//             output +=  currunt.val + '->';
-//             currunt = currunt.next;
-//         }
-//         output += currunt.val + '->null';
-//         // console.log(this.front);
-//         return output;
-//     }
-// }
-
 class Circular_queue_linked_list {
     constructor(size) {
         this.size = size;
