bc52a8e504e636bed903dc30e03b5d1bd8a999dd
aadil42 <adilkelawala@gmail.com>
2022-03-24 15:29:04 +0530

    overwrote the previous methods enqueu, isFull, and empty. Implementing the circular linklist beforehand of size n and then enquing and dequing

--- Data-structure/circular-queue-linked-list/circular-queue-linked-list.js
+++ Data-structure/circular-queue-linked-list/circular-queue-linked-list.js
@@ -13,45 +13,117 @@ class Node {
     }
 }
 
+// class Circular_queue_linked_list {
+//     constructor(size) {
+//         this.size = size;
+//         this.front = null;
+//         this.rear = null;
+//         this.counter = 0;
+//     }
+//     // 1->2->3->4  5
+//     enqueue(data) {
+//         if(this.isFull()) {
+//             return;
+//         } else if(this.empty()) {
+//             this.front = new Node(data);
+//             this.rear = this.front;
+//             this.rear.next = this.front;
+//             this.counter++;
+//         } else {
+//             const dataNode = new Node(data);
+//             const temp = this.rear.next;
+//             this.rear.next = dataNode;
+//             this.rear = dataNode;
+//             dataNode.next = temp; 
+//             this.counter++;
+//         }
+//     }
+//     dequeu() {
+//         this.front = this.front.next;
+//         this.counter--;
+//     }
+//     empty() {
+//         if(this.counter === 0) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+
+//     isFull() {
+//         if(this.counter === this.size) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+
+//     print() {
+//         let output = '';
+//         let currunt = this.front;
+//         while(currunt.next !== this.front) {
+//             output +=  currunt.val + '->';
+//             currunt = currunt.next;
+//         }
+//         output += currunt.val + '->null';
+//         // console.log(this.front);
+//         return output;
+//     }
+// }
+
 class Circular_queue_linked_list {
     constructor(size) {
         this.size = size;
-        this.front = null;
-        this.rear = null;
         this.counter = 0;
+        this.queue = this.front;
+
+        // null->null->null->null
+        this.front = new Node(null);
+        let temp = this.front;
+        let i = 1;
+        while(i < this.size) {
+            const temp1 = new Node(null);
+            this.front.next = temp1;
+            this.front = this.front.next;
+            i++;
+        }
+        this.front.next = temp;
+        this.front = temp;
+        this.rear = temp;
     }
-    // 1->2->3
+    //1->2->3->4
     enqueue(data) {
+        
         if(this.isFull()) {
             return;
         } else if(this.empty()) {
-            this.front = new Node(data);
-            this.rear = this.front;
-            this.rear.next = this.front;
-            this.counter++;
+            this.rear.val = data;
+            this.rear = this.rear.next;
         } else {
-            const dataNode = new Node(data);
-            const temp = this.rear.next;
-            this.rear.next = dataNode;
-            this.rear = dataNode;
-            dataNode.next = temp; 
-            this.counter++;
+            this.rear.val = data;
+            this.rear = this.rear.next;
+        }
+    }
+    dequeu() {
+        if(this.front.val) {
+            this.front.val = null;
+            this.front = this.front.next;    
         }
     }
 
     empty() {
-        if(this.counter === 0) {
+        if(this.front.val == null && this.rear.val == null) {
             return true;
         } else {
             return false;
         }
     }
-
+    // 1->2->3
     isFull() {
-        if(this.counter === this.size) {
-            return true;
-        } else {
+        if(this.rear.val == null) {
             return false;
+        } else {
+            return true;
         }
     }
 
@@ -67,13 +139,17 @@ class Circular_queue_linked_list {
         return output;
     }
 }
-
-
 const myCircularQueue = new Circular_queue_linked_list(4);
 myCircularQueue.enqueue(1);
 myCircularQueue.enqueue(2);
 myCircularQueue.enqueue(3);
+myCircularQueue.dequeu();
+myCircularQueue.dequeu();
+myCircularQueue.dequeu();
+myCircularQueue.dequeu();
+myCircularQueue.dequeu();
 myCircularQueue.enqueue(4);
-// myCircularQueue.enqueue(5);
+myCircularQueue.dequeu();
+myCircularQueue.enqueue(5);
 
 console.log(myCircularQueue.print());
