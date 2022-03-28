f5644f8494973c37b999b3068d22c4a632536bb6
aadil42 <adilkelawala@gmail.com>
2022-03-28 14:15:53 +0530

    implmented reverse queue method

--- Data-structure/queue-array/queue-array.js
+++ Data-structure/queue-array/queue-array.js
@@ -5,34 +5,50 @@
 // - count() – Get number of items in the queue.
 // - peek() – return front elemenet in the queue(line).
 // 1 2 3 4 5 6 7 8 9 10 
-
-
+//   f     r
+// [ 1,2 3,4 ]
 class Queue {
     constructor(size) {
         this.queue = [];
         this.front = 0;
+        this.rear = -1;
         this.size = size;
     }
 
     enqueue(data) {
-        if(this.size > this.count()) {
+        if(!this.isFull()) {
+            this.rear++;
             this.queue.push(data);
             return this.queue;    
         }
     }
 
     dequeue() {
+       this.queue[this.front] = null;
        this.front++;
     }
 
     isEmpty() {
-        if(this.front > this.queue.length - 1) {
+        if(this.rear == 0) {
             return true;
         } else {
             return false;
         }
     }
-    
+    reverseQueue() {
+    let left = this.front;
+    let right = this.rear;
+
+    while(left < right) {
+        console.log(left, right);
+        const temp = this.queue[left];
+        this.queue[left] = this.queue[right];
+        this.queue[right] = temp;
+        left++;
+        right--;
+    }
+
+    }
     count() {
         return this.queue.length - this.front;
     }
@@ -42,11 +58,15 @@ class Queue {
     }
 
     isFull() {
-        if(this.front < this.queue.length - 1) {
-            return true;
-        }else {
+        if(this.rear < this.size - 1) {
             return false;
+        }else {
+            return true;
         }
+    }
+    print() {
+        return this.queue;
+
     }
 }
 
@@ -55,11 +75,13 @@ const myQueue = new Queue(5);
 myQueue.enqueue(1);
 myQueue.enqueue(2);
 myQueue.enqueue(3);
-// myQueue.dequeue();
+myQueue.dequeue();
 myQueue.enqueue(4);
-myQueue.enqueue(5);
-myQueue.enqueue(6);
+// myQueue.enqueue(5);
+// myQueue.enqueue(6);
+// myQueue.enqueue(6);
+myQueue.reverseQueue();
 // myQueue.dequeue();
 // myQueue.dequeue();
 
-console.log(myQueue.count());
+console.log(myQueue.print());
