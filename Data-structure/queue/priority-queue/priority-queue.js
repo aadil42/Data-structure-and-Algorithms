be734f9b5eea6ef8a99798356d1ad82f9bc167ef
aadil42 <adilkelawala@gmail.com>
2022-04-25 17:25:31 +0530

    removed push and pop method, Will implement them again properly

--- Data-structure/queue/priority-queue/priority-queue.js
+++ Data-structure/queue/priority-queue/priority-queue.js
@@ -6,48 +6,50 @@
 // swap done
 // peek  done
 // print done
-
+// this is a max heap
 class PriorityQueue {
     constructor() {
         this.queue = [];
         this.size = 0;
     }
 
-    push(data) {
-        this.queue.push(data);
-        let currunt_index = this.queue.length -1;
-        while(this.queue[currunt_index] > this.queue[this.getParent(currunt_index)]) {
-            const parentIn = this.getParent(currunt_index);
-            this.swap(parentIn, currunt_index);
-            currunt_index = parentIn;
-        }
-        this.size++;
-    }
+    
+    // push(data) {
+    //     this.queue.push(data);
+    //     let currunt_index = this.queue.length -1;
+    //     while(this.queue[currunt_index] > this.queue[this.getParent(currunt_index)]) {
+            
+    //         const parentIn = this.getParent(currunt_index);
+    //         this.swap(parentIn, currunt_index);
+    //         currunt_index = parentIn;
+    //     }
+    //     this.size++;
+    // }
 
-    pop() {
-        this.swap(0, this.queue.length -1);
-        this.queue.pop();
-        let currunt_index = 0;
-        while((currunt_index < this.size - 1) && 
-               this.queue[currunt_index] < this.queue[this.getLeftChild(currunt_index)] ||
-               this.queue[currunt_index] < this.queue[this.getRightChild(currunt_index)] 
-        )  {
-            let appropriateIndex;
-            if(this.queue[this.getLeftChild(currunt_index)] > this.queue[this.getRightChild(currunt_index)]) {
-                appropriateIndex = this.getLeftChild(currunt_index);
-            } else {
-                appropriateIndex = this.getRightChild(currunt_index);
-            }
-            console.log(appropriateIndex);
-           if(this.queue[currunt_index] < this.queue[appropriateIndex]) {
-               this.swap(currunt_index, appropriateIndex);
-               currunt_index = appropriateIndex;
-           } else {
-               break;
-           }
-        } 
+    // pop() {
+    //     this.swap(0, this.queue.length -1);
+    //     this.queue.pop();
+    //     let currunt_index = 0;
+    //     while((currunt_index < this.size - 1) && 
+    //            this.queue[currunt_index] < this.queue[this.getLeftChild(currunt_index)] ||
+    //            this.queue[currunt_index] < this.queue[this.getRightChild(currunt_index)] 
+    //     )  {
+    //         let appropriateIndex;
+    //         if(this.queue[this.getLeftChild(currunt_index)] > this.queue[this.getRightChild(currunt_index)]) {
+    //             appropriateIndex = this.getLeftChild(currunt_index);
+    //         } else {
+    //             appropriateIndex = this.getRightChild(currunt_index);
+    //         }
+    //         console.log(appropriateIndex);
+    //        if(this.queue[currunt_index] < this.queue[appropriateIndex]) {
+    //            this.swap(currunt_index, appropriateIndex);
+    //            currunt_index = appropriateIndex;
+    //        } else {
+    //            break;
+    //        }
+    //     } 
 
-    }
+    // }
     getLeftChild(index) {
         return (index * 2) + 1;
     }
@@ -74,18 +76,12 @@ class PriorityQueue {
 
 const myPriority = new PriorityQueue();
 
-myPriority.push(84);
-myPriority.push(33);
-myPriority.push(29);
-myPriority.push(10);
-myPriority.push(13);
-myPriority.push(87);
+myPriority.push(8.94);
+myPriority.push(10.19);
+myPriority.push(40);
+myPriority.push(15);
+
+// myPriority.pop();
 console.log(myPriority.queue);
-myPriority.pop();
-myPriority.pop();
-myPriority.pop();
-myPriority.pop();
-myPriority.pop();
-myPriority.pop();
 console.log(myPriority.queue);
 
