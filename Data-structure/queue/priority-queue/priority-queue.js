11ae733fb8f3af57e498cea6977349bf8c4b990b
aadil42 <adilkelawala@gmail.com>
2022-04-25 18:34:55 +0530

    implemented push and pop method properly

--- Data-structure/queue/priority-queue/priority-queue.js
+++ Data-structure/queue/priority-queue/priority-queue.js
@@ -13,7 +13,50 @@ class PriorityQueue {
         this.size = 0;
     }
 
+    push(data) {
+
+    this.queue.push(data);
+    let currunt_index = this.queue.length - 1;
     
+    while(currunt_index !== 0 && this.queue[currunt_index] < this.queue[this.getParent(currunt_index)]) {
+        this.swap(currunt_index, this.getParent(currunt_index));
+        currunt_index = this.getParent(currunt_index);
+    }
+    this.size++;
+    }
+
+    pop() {
+        this.swap(0, this.queue.length - 1);
+        const popData = this.queue.pop();
+
+        let i = 0;
+        while(i < this.queue.length - 1) {
+            let root = i;
+            let left = this.getLeftChild(i);
+            let right = this.getRightChild(i);
+
+            if(this.queue[root] > this.queue[left]) {
+                this.swap(root, this.getLeftChild(root));
+                i = left;
+            }
+            if(this.queue[root] > this.queue[right]) {
+                this.swap(root, this.getRightChild(root));
+                i = right;
+            }
+
+            if(this.queue[root] < this.queue[left] && this.queue[root] < this.queue[right]) {
+                break;
+            }
+
+            if(!this.queue[left] || !this.queue[right]) {
+                break;
+            }
+        }
+
+        return popData;
+    }
+
+
     // push(data) {
     //     this.queue.push(data);
     //     let currunt_index = this.queue.length -1;
@@ -76,12 +119,14 @@ class PriorityQueue {
 
 const myPriority = new PriorityQueue();
 
-myPriority.push(8.94);
-myPriority.push(10.19);
-myPriority.push(40);
-myPriority.push(15);
-
-// myPriority.pop();
+myPriority.push(4);
+myPriority.push(5);
+myPriority.push(20);
+myPriority.push(7);
 console.log(myPriority.queue);
+myPriority.pop();
+myPriority.pop();
+myPriority.pop();
+// myPriority.pop();
 console.log(myPriority.queue);
 
