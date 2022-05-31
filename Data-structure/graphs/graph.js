a3fc83b01641ddcece0190f332eaa2449771e092
aadil42 <adilkelawala@gmail.com>
2022-05-31 10:59:34 +0530

    implemented deleteConnection, updateConnection

--- Data-structure/graphs/graph.js
+++ Data-structure/graphs/graph.js
@@ -20,7 +20,9 @@ class Graph{
             return false;
         }
     }
-
+    showGraph() {
+        return this.graphList;
+    }
     addNode(id, name) {
         if(this.idExists(id)) {
             return 'id already exists try a diffrent id';
@@ -28,13 +30,13 @@ class Graph{
             this.graphList.set(id, new Node(name));
         }
     }
-
     deleteNode(id) {
         const neighbors = this.graphList.get(id).list;
         for(const [key, value] of neighbors) {
             this.graphList.get(key).list.delete(id);
             neighbors.delete(key);
         }
+        this.graphList.delete(id);
      }
 
      connectNode(id, id1, w) {
@@ -48,6 +50,26 @@ class Graph{
              node2.list.set(id, w);
          }
      }
+     updateConnection(id, id1, w) {
+        const node1 = this.graphList.get(id);
+        const node2 = this.graphList.get(id1);
+        // connection an edge
+        if(this.graphList.has(id) && this.graphList.has(id1)) {
+            const first = this.graphList.get(id);
+            const second = this.graphList.get(id1);
+            first.list.set(id1, w);
+            second.list.set(id, w);
+        } else {
+            return 'enter valid id';
+        }
+     }
+    deleteConnection(id, id1) {
+       const first = this.graphList.get(id);
+       const second = this.graphList.get(id1);
+
+       first.list.delete(id1);
+       second.list.delete(id);
+    }        
 }
 
 const myGraph = new Graph();
@@ -61,8 +83,8 @@ myGraph.connectNode(2,3,100);
 myGraph.connectNode(3,1,100);
 myGraph.connectNode(3,2,100);
 
-console.log(myGraph.graphList.get(3));
-
+console.log(myGraph.updateConnection(3, 2, 500));
+console.log(myGraph.showGraph());
 
 
 
