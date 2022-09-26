95e25a8c3b4c5e4375350ca6f20c357da98b9155
aadil42 <adilkelawala@gmail.com>
2022-09-26 17:19:26 +0530

    tested an example on this code and it faild so it's not a correct priority heap check out the priority-queue-1.js it has the correct code

--- Data-structure/queue/priority-queue/priority-queue.js
+++ Data-structure/queue/priority-queue/priority-queue.js
@@ -6,6 +6,7 @@
 // swap done
 // peek  done
 // print done
+
 // this is a max heap
 class PriorityQueue {
     constructor() {
@@ -14,56 +15,17 @@ class PriorityQueue {
     }
 
     // this is for the min heap
-    push(data) {
-    this.queue.push(data);
-    let currunt_index = this.queue.length - 1;
-    
-    while(currunt_index !== 0 && this.queue[currunt_index] < this.queue[this.getParent(currunt_index)]) {
-        this.swap(currunt_index, this.getParent(currunt_index));
-        currunt_index = this.getParent(currunt_index);
-    }
-    this.size++;
-    }
-    // this is for the min heap
-    pop() {
-        this.swap(0, this.queue.length - 1);
-        const popData = this.queue.pop();
-        let i = 0;
-        while(i < this.queue.length - 1) {
-            let root = i;
-            let left = this.getLeftChild(i);
-            let right = this.getRightChild(i);
-            if(this.queue[root] > this.queue[left]) {
-                this.swap(root, this.getLeftChild(root));
-                i = left;
-            }
-            if(this.queue[root] > this.queue[right]) {
-                this.swap(root, this.getRightChild(root));
-                i = right;
-            }
-            if(this.queue[root] < this.queue[left] && this.queue[root] < this.queue[right]) {
-                break;
-            }
-            if(!this.queue[left] || !this.queue[right]) {
-                break;
-            }
-        }
-
-        return popData;
-    }
-
-    // this is for the max heap
     // push(data) {
-    //     this.queue.push(data);
-    //     let currunt_index = this.queue.length - 1;
-    //     while(currunt_index !== 0 && this.queue[currunt_index] > this.queue[this.getParent(currunt_index)]) {
-    //         this.swap(currunt_index, this.getParent(currunt_index));
-    //         currunt_index = this.getParent(currunt_index);
-    //     }
-    //     this.size++;
+    // this.queue.push(data);
+    // let currunt_index = this.queue.length - 1;
+    
+    // while(currunt_index !== 0 && this.queue[currunt_index] < this.queue[this.getParent(currunt_index)]) {
+    //     this.swap(currunt_index, this.getParent(currunt_index));
+    //     currunt_index = this.getParent(currunt_index);
     // }
-
-    // // this is for the max heap
+    // this.size++;
+    // }
+    // // this is for the min heap
     // pop() {
     //     this.swap(0, this.queue.length - 1);
     //     const popData = this.queue.pop();
@@ -72,17 +34,15 @@ class PriorityQueue {
     //         let root = i;
     //         let left = this.getLeftChild(i);
     //         let right = this.getRightChild(i);
-    //         if(this.queue[root] < this.queue[left]) {
+    //         if(this.queue[root] > this.queue[left]) {
     //             this.swap(root, this.getLeftChild(root));
     //             i = left;
     //         }
-
-    //         // now the root will have the value of left child.
-    //         if(this.queue[root] < this.queue[right]) {
+    //         if(this.queue[root] > this.queue[right]) {
     //             this.swap(root, this.getRightChild(root));
     //             i = right;
     //         }
-    //         if(this.queue[root] > this.queue[left] && this.queue[root] > this.queue[right]) {
+    //         if(this.queue[root] < this.queue[left] && this.queue[root] < this.queue[right]) {
     //             break;
     //         }
     //         if(!this.queue[left] || !this.queue[right]) {
@@ -92,6 +52,48 @@ class PriorityQueue {
 
     //     return popData;
     // }
+
+    // this is for the max heap
+    push(data) {
+        this.queue.push(data);
+        let currunt_index = this.queue.length - 1;
+        while(currunt_index !== 0 && this.queue[currunt_index] > this.queue[this.getParent(currunt_index)]) {
+            this.swap(currunt_index, this.getParent(currunt_index));
+            currunt_index = this.getParent(currunt_index);
+        }
+        this.size++;
+    }
+
+    // this is for the max heap
+    pop() {
+        this.swap(0, this.queue.length - 1);
+        const popData = this.queue.pop();
+        console.log(popData);
+        let i = 0;
+        while(i < this.queue.length - 1) {
+            let root = i;
+            let left = this.getLeftChild(i);
+            let right = this.getRightChild(i);
+            if(this.queue[root] < this.queue[left]) {
+                this.swap(root, this.getLeftChild(root));
+                i = left;
+            }
+
+            // now the root will have the value of left child.
+            if(this.queue[root] < this.queue[right]) {
+                this.swap(root, this.getRightChild(root));
+                i = right;
+            }
+            if(this.queue[root] > this.queue[left] && this.queue[root] > this.queue[right]) {
+                break;
+            }
+            if(!this.queue[left] || !this.queue[right]) {
+                break;
+            }
+        }
+
+        return popData;
+    }
 
     getLeftChild(index) {
         return (index * 2) + 1;
@@ -119,14 +121,30 @@ class PriorityQueue {
 
 const myPriority = new PriorityQueue();
 
-myPriority.push(8.94);
-myPriority.push(10.19);
-myPriority.push(12.20);
-myPriority.push(8.06);
+myPriority.pus(100);
+myPriority.pus(19);
+myPriority.pus(3);
+myPriority.pus(1);
+myPriority.pus(36);
+myPriority.pus(17);
+myPriority.pus(2);
+myPriority.pus(7);
+myPriority.pus(25);
+
 console.log(myPriority.queue);
 myPriority.pop();
 myPriority.pop();
 myPriority.pop();
 myPriority.pop();
+myPriority.pop();
+myPriority.pop();
+myPriority.pop();
+myPriority.pop();
+myPriority.pop();
+myPriority.pop();
+// myPriority.pop();
 console.log(myPriority.queue);
+
+
+
 
