df01ca6283ee636b67950a6ae283c7775f4df75f
aadil42 <adilkelawala@gmail.com>
2023-06-02 14:19:29 +0530

    solved array-partition

---
+++ Algorithms/01-Arrays/25-array-partition.js
@@ -0,0 +1,16 @@
+/**
+ * Sorting
+ * 
+ * Time O(n*log(n)) | Space O(n) (because of merge sort)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    nums = nums.sort((a,b) => a-b);
+    let max = 0;
+
+    for(let i = 0; i < nums.length; i += 2) {
+        max += Math.min(nums[i], nums[i+1]);
+    }
+    return max;
+};
