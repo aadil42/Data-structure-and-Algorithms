1f1d612f4def9aa39e09241d392bf60a1c386ad1
aadil42 <adilkelawala@gmail.com>
2023-06-08 18:04:52 +0530

    updated 04-quick-sort code.

--- Algorithms/02-Sorting/04-quick-sort.js
+++ Algorithms/02-Sorting/04-quick-sort.js
@@ -1,26 +1,27 @@
 
+// worst case.
 // Time O(n^2) | Space O(n^2)
+
+// average case.
+// Time O(n*log(n)) | Space O(n*log(n))
 const quickSort = (arr) => {
-    
-    const recursion = (arr) => {
+    console.log(arr, 'hehe');
+    if(arr.length <= 1) return arr;
 
-        if(arr.length <= 1) return arr;
+    const pivot = arr.pop();
 
-        const pivot = arr.pop();
+    const leftPartition = [];
+    const rightPartition = [];
+    const equalPartition = [];
 
-        const leftPartition = [];
-        const rightPartition = [];
-
-        for(let i = 0; i < arr.length; i++) {
-            if(arr[i] < pivot) leftPartition.push(arr[i]);
-            if(arr[i] > pivot) rightPartition.push(arr[i]);
-        }
-        console.log(leftPartition, rightPartition);
-        return [...recursion(leftPartition), pivot, ...recursion(rightPartition)];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] < pivot) leftPartition.push(arr[i]);
+        if(arr[i] > pivot) rightPartition.push(arr[i]);
+        if(arr[i] === pivot) equalPartition.push(arr[i]);
     }
 
-    return recursion(arr);
+    return [...quickSort(leftPartition),...equalPartition, pivot, ...quickSort(rightPartition)];
 }
 
-const arr = [6, 3, 9, 5, 2, 8];
+const arr = [6, 3, 9, 5, 2, 8, 2, 2];
 console.log(quickSort(arr));
