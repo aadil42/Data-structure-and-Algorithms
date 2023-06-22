35044812dcf5e39f95ae0325f6f4c943c78767a4
aadil42 <adilkelawala@gmail.com>
2023-06-22 19:40:58 +0530

    added brute force solution with n^2 time complexity

--- Algorithms/03-searching/problems/16-two-sum-ii-input-array-is-sorted.js
+++ Algorithms/03-searching/problems/16-two-sum-ii-input-array-is-sorted.js
@@ -27,3 +27,24 @@ var twoSum = function(numbers, target) {
 
     return result;
 };
+
+/**
+ * Brute force
+ * 
+ * Time O(n^2) | Space O(1)
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum1 = function(numbers, target) {
+    for(let i = 0; i < numbers.length; i++) {
+     for(let j = i + 1; j < numbers.length; j++) {
+         if (numbers[i] + numbers[j] === target) 
+ {
+     return [i+1, j+1];
+ }
+     }
+    } 
+     
+     return "can't obtain the sum";
+ };
