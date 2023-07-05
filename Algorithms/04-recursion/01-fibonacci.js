c19361af0f1f7ce1c6143f5cb8c9d4d31e0e86c9
aadil42 <adilkelawala@gmail.com>
2023-07-05 20:04:09 +0530

    solved fibonacci with caching

--- Algorithms/04-recursion/01-fibonacci.js
+++ Algorithms/04-recursion/01-fibonacci.js
@@ -1,10 +1,37 @@
 /**
+ * Recursion | DP
+ * https://leetcode.com/problems/fibonacci-number/
+ * 
+ * Time O(n) | Space O(n)
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    
+    const hash = {};
+    const dfs = (n) => {
+        if(hash[n]) return hash[n];
+        if(n <= 0) {
+            return 0;
+        }
+        if(n === 1) {
+            return 1;
+        }
+        const result =  dfs(n-1) + dfs(n-2);
+        hash[n] = result;
+        return result;
+    }
+
+    return dfs(n);
+};
+
+/**
  * Brute force
  * Time O(2^n) | Space O(n)
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fib1 = function(n) {
     
     const dfs = (n) => {
         if(n <= 0) {
