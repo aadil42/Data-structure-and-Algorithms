aae3ce2f6b945e553584214f15261a79b3c13910
aadil42 <adilkelawala@gmail.com>
2023-07-06 20:27:10 +0530

    solved 02-reverse-string with recrusion

---
+++ Algorithms/04-recursion/02-reverse-string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+
+    const dfs = (left, right) => {
+        if(left >=  right) return;
+        swap(s, left, right);
+        dfs(left+1, right-1);
+    }
+
+    dfs(0, s.length - 1);
+};
+
+ function swap(s,left,right) {
+     const temp = s[left];
+     s[left] = s[right];
+     s[right] = temp;
+ }
